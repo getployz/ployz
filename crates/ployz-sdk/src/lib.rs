cf3855fc4917db4419c72d31f68160933bf7a86a
Nick Potts <github@nickpotts.com.au>
2026-03-19 09:27:48 +0800

    done

--- crates/ployz-sdk/src/lib.rs
+++ crates/ployz-sdk/src/lib.rs
@@ -6,5 +6,4 @@ pub use ployz_api::{
     DaemonRequest, DaemonResponse, DeployOptions, InstallSource, MachineAddOptions,
     MachineInstallOptions, MachineListPayload, MeshReadyPayload, MeshSelfRecordPayload,
 };
-pub use ployz_runtime_api::DeployFrame;
 pub use transport::{StdioTransport, Transport, UnixSocketTransport};
